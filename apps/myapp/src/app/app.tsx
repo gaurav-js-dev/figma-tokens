391d4fbc5dd551405e486980e3d87f4b50d8ee03
Gaurav <23628920+gaurav-js-dev@users.noreply.github.com>
2023-04-05 00:00:55 +0530

    Initiated Boilerplate with React Project

---
+++ apps/myapp/src/app/app.tsx
@@ -0,0 +1,38 @@
+import './app.scss';
+
+export function App() {
+  return (
+    <>
+      <h1>
+        <span> Hello there, </span>
+        Welcome myapp 👋
+      </h1>
+
+      <div />
+    </>
+  );
+}
+
+export default App;
+
+if (import.meta.vitest) {
+  // add tests related to your file here
+  // For more information please visit the Vitest docs site here: https://vitest.dev/guide/in-source.html
+
+  const { it, expect, beforeEach } = import.meta.vitest;
+  let render: any;
+
+  beforeEach(async () => {
+    render = (await import('@testing-library/react')).render;
+  });
+
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a greeting as the title', () => {
+    const { getByText } = render(<App />);
+    expect(getByText(/Welcome myapp/gi)).toBeTruthy();
+  });
+}
